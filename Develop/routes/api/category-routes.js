553354ccef1d4b7a3560e0e561bd37f7265c1be2
Suvarna Jadhav <jadhav.suvarna28@gmail.com>
2023-02-16 11:02:46 -0500

    added create/post category and update/put category in category routes js file

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -33,12 +33,28 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    })
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // delete one category by id
@@ -51,7 +67,7 @@ router.delete('/:id', async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No category found with this id!' });
+      res.status(404).json({ message: 'No product found with this id!' });
       return;
     }
 
